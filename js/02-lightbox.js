029580b310b473c98e9ded22ef8c42738c0fbd61
Anastasiia-Ihn <128808242+Anastasiia-Ihn@users.noreply.github.com>
2023-08-04 07:46:18 +0200

    added task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,32 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryEl = document.querySelector('.gallery')
+
+galleryEl.insertAdjacentHTML('afterbegin', createCardsMarkup(galleryItems))
+galleryEl.addEventListener('click', handlerClickCard)
+
+function createCardsMarkup(arr) {
+     return arr.map(({preview, original, description}) => `
+<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`).join('')
+}
+
+function handlerClickCard(evt) { 
+evt.preventDefault()   
+if (evt.target === evt.currentTarget) {
+  return 
+} 
+    let lightbox = new SimpleLightbox('.gallery a', { 'captionsData': 'alt', 'captionDelay': 250,});
+
+   
+}
